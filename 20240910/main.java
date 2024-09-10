ce95fede39f8e2e3883169da0e0a931aaa00b851
shshsheep <tung.lung38@gmail.com>
2024-09-10 21:59:30 +0800

    first class
    編譯環境

---
+++ 20240910/main.java
@@ -0,0 +1,17 @@
+//課程檔案
+//https://drive.google.com/drive/folders/1nbM7Klok0UGTwVbcCngLbk_ZPP8mWJwV
+
+//設定環境變數
+//新增
+//變數名稱JAVA_HOME
+//變數目錄C:\JAVA\JDK11(檔案目錄)
+//變數名稱CLASSPATH
+//變數目錄.;%JAVA_HOME%\lib
+//編輯新增Path
+//%JAVA_HOME%\bin 並上移至最上方
+
+//驗證JAVA版本 javac -version 驗證JDK
+//驗證JAVA版本 java -version 驗證JRE
+
+//直接將netbeam貼在C:/上面
+//netbeam/bin/64.exe
